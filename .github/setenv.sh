953bc99cc2e9298c45dc57e2cba09c7cad56f491
anvie <robinsyihab@gmail.com>
2025-08-24 13:20:43 +0700

    remove github workflow

--- .github/setenv.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# echo "::set-env name=VERSION::$(cat Cargo.toml | grep version | head -n 1 | cut -d '"' -f 2)"
-
-VERSION=$(cat Cargo.toml | grep version | head -n 1 | cut -d '"' -f 2)
-
-echo "VERSION=$VERSION"
-echo "BIN_OUTPUT=/usr/local/bin/frappe_mcp-$VERSION"
-
